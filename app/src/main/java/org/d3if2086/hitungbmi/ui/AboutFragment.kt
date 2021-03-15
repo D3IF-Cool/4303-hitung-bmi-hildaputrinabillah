2f8eecdaf285b3874e367f8c628bf938f71f2587
hildaputrinabillah <hildaputrinabillah@student.telkomuniversity.ac.id>
2021-03-15 12:37:11 +0700

    3.6.Membuat navigasi menggunakan menu

---
+++ app/src/main/java/org/d3if2086/hitungbmi/ui/AboutFragment.kt
@@ -0,0 +1,5 @@
+package org.d3if2086.hitungbmi.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if2086.hitungbmi.R
+class AboutFragment : Fragment(R.layout.fragment_about)
--- app/src/main/java/org/d3if2086/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if2086/hitungbmi/ui/HitungFragment.kt
@@ -2,17 +2,30 @@ package org.d3if2086.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if2086.hitungbmi.R
 import org.d3if2086.hitungbmi.data.KategoriBmi
 import org.d3if2086.hitungbmi.databinding.FragmentHitungBinding
 
 class HitungFragment : Fragment() {
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                    R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
     private lateinit var binding: FragmentHitungBinding
     private lateinit var kategoriBmi: KategoriBmi
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +38,7 @@ class HitungFragment : Fragment() {
                 HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
     private fun hitungBmi() {
