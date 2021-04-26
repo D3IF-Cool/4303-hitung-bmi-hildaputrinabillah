c02f419583fbe51d563198c2eb0379894210ac68
hildaputrinabillah <hildaputrinabillah@student.telkomuniversity.ac.id>
2021-04-26 07:36:11 +0700

    3.8.Menghapus data dari database

--- app/src/main/java/org/d3if2086/hitungbmi/db/BmiDao.kt
+++ app/src/main/java/org/d3if2086/hitungbmi/db/BmiDao.kt
@@ -12,5 +12,7 @@ interface BmiDao {
     fun insert(bmi: BmiEntity)
     @Query("SELECT * FROM bmi ORDER BY id DESC")
     fun getLastBmi(): LiveData<List<BmiEntity>>
+    @Query("DELETE FROM bmi")
+    fun clearData()
 
 }
